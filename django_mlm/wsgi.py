fcc35be3d83fc5d9e1a2f17014061816a51a6f8e
Marius Safta <marius.safta@ratiopartners.co.uk>
2020-10-09 14:17:30 +0300

    Second ML algorithm

--- django_mlm/wsgi.py
+++ django_mlm/wsgi.py
@@ -19,15 +19,11 @@ application = get_wsgi_application()
 Add ML algorithms to the registry when the server is starting.
 """
 
-# import os
-# from django.core.wsgi import get_wsgi_application
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-# application = get_wsgi_application()
-
 # ML registry
 import inspect
 from ML.registry import MLRegistry
 from ML.income_classifier.random_forest import RandomForestClassifier
+from ML.income_classifier.extra_trees import ExtraTreesClassifier
 
 try:
     registry = MLRegistry() # create ML registry
@@ -41,6 +37,18 @@ try:
                            algorithm_version="0.0.2",
                            owner="Marius",
                            algorithm_description="Random Forest with simple pre- and post-processing",
+                           algorithm_code=inspect.getsource(RandomForestClassifier))
+
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                           algorithm_object=et,
+                           algorithm_name="extra trees",
+                           algorithm_status="testing",
+                           algorithm_version="0.0.1",
+                           owner="Piotr",
+                           algorithm_description="Extra Trees with simple pre- and post-processing",
                            algorithm_code=inspect.getsource(RandomForestClassifier))
 
 except Exception as e:
